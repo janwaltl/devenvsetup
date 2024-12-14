75c3cf4d8549d383a6ba9ba32bd7b809b12215e9
Jan Waltl <waltl.jan@gmail.com>
2024-12-14 15:49:26 +0100

    chore: persistent neovim build dir

--- install_neovim.sh
+++ install_neovim.sh
@@ -6,15 +6,16 @@ set -e
 sudo apt-get install liblua5.1-dev luajit libluajit-5.1-dev libperl-dev libncurses5-dev ruby-dev
 
 
-CLONE_DIR=/tmp/neovim.build
+CLONE_DIR="${HOME}/nvim/"
 
-if [ ! -d ${CLONE_DIR} ];then
+if [ ! -d "${CLONE_DIR}" ];then
 	git clone --depth 1 https://github.com/neovim/neovim "${CLONE_DIR}"
 fi
 
 cd "${CLONE_DIR}"
+git pull
 # Build
-make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=$HOME/.local/
+make CMAKE_BUILD_TYPE=RelWithDebInfo "CMAKE_INSTALL_PREFIX=${HOME}/.local/"
 
 # Install
 make install
